5097082c982fc9f97e6f68ac68145877786ff172
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-02-04 13:50:05 +0100

    Add make_tags function

--- TestClass.py
+++ TestClass.py
@@ -97,5 +97,8 @@ def hello_name(name):
 
 def make_abba(a,b):
     return a+b+b+a
+
+def make_tags(tag, word):
+    return "<"+tag+">"+word+"</"+tag+">"
 ##Call the method
 print(make_abba("Tomek","Cool"))
