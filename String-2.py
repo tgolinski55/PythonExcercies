23d0f9427dca6f3a550d6a1868926a469a062479
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-02-19 17:16:49 +0100

    Add count_code function

--- String-2.py
+++ String-2.py
@@ -24,4 +24,17 @@ def cat_dog(str):
     else:
         return False
 
-print(cat_dog("catxxdogxxxdog"))
+# Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+
+# count_code('aaacodebbb') → 1
+# count_code('codexxcode') → 2
+# count_code('cozexxcope') → 2
+
+def count_code(str):
+    countStr = 0
+    for i in range(len(str)-3):
+        if str[i:i+2]=="co" and str[i+3]=="e":
+            countStr += 1
+    return countStr
+
+print(count_code("cozexxcope"))
