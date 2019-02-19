a1141ec19b72ee182c1a6e586a63754f57f33da1
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-02-19 16:28:57 +0100

    Add cat_dog function

--- String-2.py
+++ String-2.py
@@ -11,4 +11,17 @@ def count_hi(str):
             count += 1
     return count
 
-print(count_hi("WordHihihihihihHi"))
+def cat_dog(str):
+    catCount = 0
+    dogCount = 0
+    for i in range(len(str)-2):
+        if str[i:i+3]=="cat":
+            catCount += 1
+        if str[i:i+3]=="dog":
+            dogCount += 1
+    if catCount == dogCount:
+        return True
+    else:
+        return False
+
+print(cat_dog("catxxdogxxxdog"))
