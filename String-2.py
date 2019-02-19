229353087b40a25ae9bcf46aacd9da5c642b1472
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-02-19 16:18:21 +0100

    Add count_hi function

--- String-2.py
+++ String-2.py
@@ -4,4 +4,11 @@ def double_char(str):
         word+=str[n]+str[n]
     return word
 
-print(double_char("Word"))
+def count_hi(str):
+    count = 0
+    for n in range(len(str)-1):
+        if str[n]=="h" and str[n+1]=="i":
+            count += 1
+    return count
+
+print(count_hi("WordHihihihihihHi"))
