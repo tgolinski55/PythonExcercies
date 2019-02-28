d7a7a29cb1cb493ff84b4b161646cb7b9ba82bef
Tomasz Goliński - laptop <tgolinski55@gmail.com>
2019-02-28 10:46:07 +0100

    Partial solution for centered_average function

--- List-2.py
+++ List-2.py
@@ -8,4 +8,25 @@ def count_evens(nums):
 def big_diff(nums):
     return max(nums)-min(nums)
 
-print(big_diff([1,2,3,5,2]))
+def centered_average(nums):
+    x = min(nums)
+    y = max(nums)
+    skippedX = 0
+    skippedY = 0
+    temp = []
+    for i in range(len(nums)):
+        if (nums[i]!=x or skippedX<=1) and (nums[i]!=y or skippedY<=1):
+            temp.append(nums[i])
+        elif nums[i]==x:
+            skippedX+=1
+        elif nums[i]==y:
+            skippedY+=1
+    sumVal = 0
+    for n in range(len(temp)):
+        sumVal += temp[n]
+    if len(temp)>0:
+        return int(sumVal/len(temp))
+    else:
+        return 0
+
+print(centered_average([1,1,100]))
